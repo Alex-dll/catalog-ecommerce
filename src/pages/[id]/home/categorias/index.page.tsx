76b21b3257e924cb76c106548809b8e73072e7e2
alex-dll <alexsandrojr.303@gmail.com>
2022-07-13 13:02:37 -0300

    feat: :sparkles: introduce crut to categories

---
+++ src/pages/[id]/home/categorias/index.page.tsx
@@ -0,0 +1,35 @@
+import { DashboardCategories } from "components";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { CatalogAtCompanyProps } from "types";
+import { getCatalog, getCompany } from "utils/http";
+
+interface Props {
+  corporateName: string;
+  catalogCompany: CatalogAtCompanyProps;
+}
+
+export default function Categories() {
+  return (
+    <>
+      <DashboardCategories />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params?.id as string;
+  const dataCompany = await getCompany(id);
+  const catalogCompany = await getCatalog({
+    companyId: dataCompany.id,
+    maxItemsPage: 20,
+    pageNumber: 1,
+  });
+  return {
+    props: {
+      corporateName: dataCompany.corporateName,
+      catalogCompany,
+    },
+  };
+};
